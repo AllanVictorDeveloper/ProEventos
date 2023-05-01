8727d017d36d6f67d1d055ce874498e8e4898fee
Allan Santos <developer.allansantos@outlook.com>
2023-04-30 21:28:18 -0300

    refatorando o endpoints evento

--- ProEventos.Api/Controllers/EventosController.cs
+++ ProEventos.Api/Controllers/EventosController.cs
@@ -95,7 +95,7 @@ namespace ProEventos.Api.Controllers
                 if (retorno.Result == null)
                     BadRequest(new { mensagem = "Não foi possivel adicionar evento." });
 
-                return Ok(new { mensagem = "Evento adicionado com sucesso" });
+                return Ok(new { mensagem = "Evento adicionado com sucesso", status = 200 });
             }
             catch (Exception ex)
             {
